f1fc03e4d5dba4fcfa6402cee34a4ef8be800b4b
honeynut48 <singhtina2003@gmail.com>
2026-02-18 22:16:42 -0500

    updated to include AJAX

--- myscript.js
+++ myscript.js
@@ -8,3 +8,40 @@ $('.carousel').carousel({
     interval: 2000,
     pause: "hover"
 })
+
+
+const images = [
+    "images/astrantia.jpg",
+    "images/geranium.jpg",
+    "images/sanguineum.jpg",
+    "images/sulphurea.jpg"
+];
+
+const descriptions = [
+    "descriptions/astrantiaDesc.txt",
+    "descriptions/geraniumDesc.txt",
+    "descriptions/sanguineumDesc.txt",
+    "descriptions/sulphureaDesc.txt"
+];
+
+let currentIndex = 0;
+
+function loadContent() {
+    $("#images").attr("src", images[currentIndex]);
+    $.ajax({
+        url: descriptions[currentIndex],
+        success: function(data) {
+            $("#imageDescriptions").text(data);
+        }
+    });
+
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+$(document).ready(function () {
+    loadContent();
+    setInterval(loadContent, 2000);
+});
+
+
+
