fac9236547aeceafc2c66d6d2a998bbf42815224
honeynut48 <singhtina2003@gmail.com>
2026-02-20 12:54:12 -0500

    json added about cars

--- myscript.js
+++ myscript.js
@@ -30,8 +30,9 @@ function loadContent() {
     $("#images").attr("src", images[currentIndex]);
     $.ajax({
         url: descriptions[currentIndex],
-        success: function(data) {
-            $("#imageDescriptions").text(data);
+        dataType: "text",
+        success: function (fileText) {
+            $("#descriptions").text(fileText);
         }
     });
 
@@ -43,5 +44,42 @@ $(document).ready(function () {
     setInterval(loadContent, 2000);
 });
 
+$.ajax({
+    type:"GET",
+    url:"car.json",
+    success:function(response)
+    {
+       displayCar(response);
+    }
+});
+
+function displayCar(response) {
+    const container = $("#carContainer");
+    const carElement = `<div class="col-8 font-weight-bold">
+        ${response.name}
+        </div>
+        <div class="col-4">
+        ${response.year}
+        </div>
+
+        <div class="col-12 text-center">
+        <img src="${response.imageURL}" alt="${response.name}" class="img-fluid">
+        </div>
+
+        <div class="col-4 font-weight-bold">
+        Engine
+        </div>
+        <div class="col-8">
+        Type: ${response.engine.type}, Size: ${response.engine.size}, Configuration: ${response.engine.configuration}
+        </div>
+
+        <div class="col-4 font-weight-bold">
+        Brakes
+        </div>
+        <div class="col-8">
+        Front: ${response.brakes.front}, Back: ${response.brakes.back}
+        </div>`;
+        container.append(carElement);
+    };
 
 
